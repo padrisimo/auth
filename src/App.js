7862e4ebc62ff984201af5f1b040b00e20f25e80
adrian.fernandez.externo@axa-assistance.es <adrian.fernandez.externo@axa-assistance.es>
2018-01-17 12:31:01 +0100

    first shot

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Ou yea!
+                </Text>
+                <Text>
+                    Ou yea!
+                </Text>
+                <Text>
+                    Ou yea!
+                </Text>
+                <Text>
+                    Ou yea!
+                </Text>
+            </View>
+        );
+    }
+}
+
+export default App;
